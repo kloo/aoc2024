bf50cd4a08eeadc289a6ddce5a5152dcdd4df95b
kloo <tehpiratezor@gmail.com>
2024-11-30 19:20:18 -1000

    Initial Setup

---
+++ 01/b_2024.ps1
@@ -0,0 +1,23 @@
+<#
+
+#>
+
+$strarray = Get-Content ina.txt
+$answer = 0
+
+$inputLen = $strarray.Length
+$inputArray = New-Object int[] $inputLen
+
+Function Test-Func {
+    Param (
+        [System.Collections.Specialized.OrderedDictionary] $playerOneDeck,
+        [System.Collections.Specialized.OrderedDictionary] $playerTwoDeck
+    )
+    
+}
+
+foreach ($line in $strarray) {
+    
+}
+
+Write-Host $answer
